05aa95ce3b7872d669d906571f0ca91180e5c1d6
Ryan Drew <learnitall0@gmail.com>
2021-12-21 09:07:10 -0700

    Move machine factory fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Store shared fixtures for foremanlite tests."""
+import random
+import typing as t
+
 import pytest
 
 import foremanlite.logging
+from foremanlite.machine import Arch, Machine
+
+TEST_MACHINES: t.Dict[str, t.Sequence] = {
+    "name": (
+        "machine1",
+        "mymachine",
+        "testmachine",
+    ),
+    "mac": ("11:22:33:44:55:66", "de:ad:be:ef:11:22", "12:34:56:78:90:12"),
+    "arch": tuple(e.value for e in Arch),
+    "provision": (True, False),
+}
 
 
 @pytest.fixture()
@@ -13,3 +28,24 @@ def logfix():
     foremanlite.logging.setup(verbose=True, use_stream=True)
     yield
     foremanlite.logging.teardown()
+
+
+def get_test_machine(**kwargs) -> Machine:
+    """
+    Generate and return new random test Machine.
+
+    kwargs can be used as universal overrides.
+    """
+    params: t.Dict[str, t.Any] = {
+        key: random.choice(value) for key, value in TEST_MACHINES.items()
+    }
+    params.update(kwargs)
+
+    return Machine(**params)
+
+
+@pytest.fixture()
+def machine_factory():
+    """Return `get_test_machine` function for generating new Machines."""
+
+    return get_test_machine
